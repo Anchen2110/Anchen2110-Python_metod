151421fb3a77b49a185a787863c481a5d690e52c
anna <egoshina_a@itstep.academy>
2022-03-02 19:39:42 +0200

    quickSort

--- 1.py
+++ 1.py
@@ -1,53 +1,110 @@
-def MergeSort(myList):
-    if len(myList) > 1:
-  
-         # Finding the middle of the list
-        m = len(myList)//2
-        print("m: {}".format(m))
-        
-  
-        # Splitting a list into left and right parts
-        leftPart = myList[:m]
-        rightPart = myList[m:]
-        print("left: {}".format(leftPart))
-        print("right: {}".format(rightPart))
-  
-        # Sorting the left part
-        MergeSort(leftPart)
-  
-        # Sorting the right part
-        MergeSort(rightPart)
-  
-        #merge steps
-        i = j = k = 0
-  
-        # Copy data to sorted list from leftPart-list and rightPart-list
-        while i < len(leftPart) and j < len(rightPart):
-            if leftPart[i] < rightPart[j]:
-                myList[k] = leftPart[i]
-                i += 1
-            else:
-                myList[k] = rightPart[j]
-                j += 1
-            k += 1
-  
-        # Checking if any element was left in leftPart-list
-        while i < len(leftPart):
-            myList[k] = leftPart[i]
-            i += 1
-            k += 1
-        # Checking if any element was left in rightPart-list
-        while j < len(rightPart):
-            myList[k] = rightPart[j]
-            j += 1
-            k += 1
-        print("temp merge: {}".format(myList))
+def QuickSort (myList):
+    if len(myList) == 1:
+        return myList
+    elif len(myList) > 1:
+        # midInd  = len(myList)//2
+        # print("midInd : {}, mid elem={}".format(midInd,myList[midInd]))
 
-numbers=[35,22,41,4,10,80,11]
+        firstInd =0
+        lastInd=len(myList)-1
+
+        midInd  = (firstInd+lastInd)//2
+        print("midInd : {}, mid elem={}".format(midInd,myList[midInd]))
+
+
+        leftPart = myList[:midInd+1]
+        rightPart = myList[midInd+1:]
+        print("current list before: {}".format(myList))
+        print("left before: {}".format(leftPart))
+        print("right before: {}".format(rightPart))
+
+        while True:
+            while myList[firstInd]<myList[midInd]:
+                firstInd+=1
+            while myList[lastInd]>myList[midInd]:
+                lastInd-=1
+            if firstInd<lastInd:
+                temp=myList[firstInd]
+                myList[firstInd]=myList[lastInd]
+                myList[lastInd]=temp
+
+                firstInd+=1
+                lastInd-=1
+            if firstInd>=lastInd:
+                break
+        print("current list after: {}".format(myList))
+        leftPart = myList[:midInd+1]
+        rightPart = myList[midInd+1:]
+        print("left after: {}".format(leftPart))
+        print("right after: {}".format(rightPart))
+
+        QuickSort (leftPart)
+        QuickSort (rightPart)
+
+        myList=leftPart+rightPart
+        
+        return myList
+
+
+
+numbers=[12,8,25,17,33,31,40,42]
 print("Original list: {}".format(numbers))
 
-MergeSort(numbers)
-print("Sorted list: {}".format(numbers))
+
+print("Sorted list: {}".format(QuickSort(numbers)))
+
+
+
+        
+
+
+# def MergeSort(myList):
+#     if len(myList) > 1:
+  
+#          # Finding the middle of the list
+#         m = len(myList)//2
+#         print("m: {}".format(m))
+        
+  
+
+#         # Splitting a list into left and right parts
+#         leftPart = myList[:m]
+#         rightPart = myList[m:]
+#         print("left: {}".format(leftPart))
+#         print("right: {}".format(rightPart))
+  
+#         # Sorting the left part
+#         MergeSort(leftPart)
+  
+#         # Sorting the right part
+#         MergeSort(rightPart)
+  
+#         #merge steps
+#         i = j = k = 0
+  
+#         # Copy data to sorted list from leftPart-list and rightPart-list
+#         while i < len(leftPart) and j < len(rightPart):
+#             if leftPart[i] < rightPart[j]:
+#                 myList[k] = leftPart[i]
+#                 i += 1
+#             else:
+#                 myList[k] = rightPart[j]
+#                 j += 1
+#             k += 1
+  
+#         # Checking if any element was left in leftPart-list
+#         while i < len(leftPart):
+#             myList[k] = leftPart[i]
+#             i += 1
+#             k += 1
+#         # Checking if any element was left in rightPart-list
+#         while j < len(rightPart):
+#             myList[k] = rightPart[j]
+#             j += 1
+#             k += 1
+#         print("temp merge: {}".format(myList))
+
+
 
 
 
