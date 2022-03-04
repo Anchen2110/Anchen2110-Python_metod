44d6d8d39a4a8009cafd41520e491362d363bd4b
anna <egoshina_a@itstep.academy>
2022-03-04 21:25:09 +0200

    tuple

---
+++ tuple.py
@@ -0,0 +1,41 @@
+# myEmptyTuple1=()
+# myEmptyTuple2= tuple()
+
+# print(myEmptyTuple1)
+# print(type(myEmptyTuple1))
+
+# print(myEmptyTuple1)
+# print(type(myEmptyTuple2))
+
+# myTuple1=('element1', 12, 35.6, False)
+# myTuple2=('item1')
+# userTypes='admin','student','teacher'
+# userName='Jane',
+
+# print("myTuple1:", myTuple1,"type: ", type(myTuple1))
+# print("myTuple2:", myTuple2,"type: ", type(myTuple2))
+# print("userTypes:", userTypes,"type: ", type(userTypes))
+# print("userName:", userName,"type: ", type(userName))
+
+# itemTuple=('item1','item2','item1','item3')
+# print(itemTuple) #('item1', 'item2', 'item1', 'item3')
+
+# namesTuple = tuple(('Alex', 'Helen'))
+# print(namesTuple) #('Alex', 'Helen')
+
+userTypes=('admin','student','teacher','moderator')
+user1, user2,user3,user4 =userTypes
+print(user1)
+print(user2)
+print(user1)
+print(user1)
+
+
+
+# print("1st user:", userTypes[0])
+# print("last user:", userTypes[len(userTypes)-1])
+# print("last user once again:", userTypes[-1])
+# print("1st two users:", userTypes[:2])
+# print("2nd and 3rd users:", userTypes[1:3])
+
+
