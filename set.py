0c269b3cb3f4005115cf909b36df3cb22081fc92
anna <egoshina_a@itstep.academy>
2022-03-07 19:58:58 +0200

    set1

---
+++ set.py
@@ -0,0 +1,11 @@
+mySet1 = {1, 2, 3}
+mySet2= {'Joe','Bob','Kate'}
+mySet3= {23,'Bob',False,(45.6, 12)}
+
+print("mySet1 - set of numbers:",mySet1)
+print("mySet2 - set of strings:",mySet2)
+print("mySet3 - of mixed datatypes:",mySet3)
+
+mySet4=set((10,20,30))
+print("mySet4:",mySet4) #
+
