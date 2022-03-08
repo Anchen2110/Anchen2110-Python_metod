f92abdb453eb7ee855a9aee970c287dd91ed545e
anna <egoshina_a@itstep.academy>
2022-03-08 15:52:19 +0200

    8_3

--- set.py
+++ set.py
@@ -1,11 +1,85 @@
-mySet1 = {1, 2, 3}
-mySet2= {'Joe','Bob','Kate'}
-mySet3= {23,'Bob',False,(45.6, 12)}
 
-print("mySet1 - set of numbers:",mySet1)
-print("mySet2 - set of strings:",mySet2)
-print("mySet3 - of mixed datatypes:",mySet3)
 
-mySet4=set((10,20,30))
-print("mySet4:",mySet4) #{10, 20, 30}
+# mySet = {1, 2, 3, 4, 5}
+# print(mySet)
+
+# mySet.discard(4)
+# print(mySet)
+
+# mySet.remove(5)
+# print(mySet)
+
+# mySet.discard(10)
+# print(mySet)
+
+
+# mySet.remove(10)
+# print(mySet)
+
+
+
+
+
+
+
+# mySet = {1, 2, 3}
+# print(mySet)
+
+# mySet.add(4)
+# print(mySet)
+
+# mySet.update([3, 4, 5])
+# print(mySet)
+
+# mySet.update([5, 6, 7], {8, 9})
+# print(mySet)
+
+
+# userNames= {'Joe','Bob','Kate'}
+# name=input("Your name:")
+# if name in userNames:
+#     print("Welcome, ", name)
+
+
+# userNames= {'Joe','Bob','Kate'}
+# for name in userNames:
+#     print(name)
+
+# word=input("Your word:")
+# for letter in set(word):
+#     print(letter)
+
+# mySet1 = {1, 2, 3}
+# mySet2= {'Joe','Bob','Kate'}
+# mySet3= {23,'Bob',False,(45.6, 12)}
+
+# print("mySet1 - set of numbers:",mySet1)
+# print("mySet2 - set of strings:",mySet2)
+# print("mySet3 - of mixed datatypes:",mySet3)
+
+# mySet4=set((10,20,30))
+# print("mySet4:",mySet4) #{10, 20, 30}
+
+# uniqueUsersName= {'Joe','Bob','Kate','Bob'}
+# print(uniqueUsersName) #{'Kate', 'Joe', 'Bob'}
+
+# allUsersName= ['Joe','Bob','Kate','Bob']
+# uniqueUsersName=set(allUsersName)
+# print(uniqueUsersName) #{'Kate', 'Joe', 'Bob'}
+
+# #passwordsSet={'111',['222','333']}
+
+# myEmptySet=set()
+# print(myEmptySet) #set()
+
+# mySetA = {1, 2, 3}
+# mySetB = {3, 2, 1}
+# print(mySetA==mySetA) #True
+
+
+
+
+
+
+
 
