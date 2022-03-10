cef3e961ac565e79c904e58c15ced31d189be3f4
anna <egoshina_a@itstep.academy>
2022-03-10 21:37:20 +0200

    createDict

---
+++ dict.py
@@ -0,0 +1,43 @@
+myEmptyDict1={}
+print(myEmptyDict1)  #{}
+
+myEmptyDict2=dict()
+print(myEmptyDict2)  #{}
+
+
+# myDict3 = dict([("a", 111), ("b", 222)])
+# myDict4 = dict([["a", 111], ["b", 222]])
+
+# myDict5 = dict(['qw', 'er', 'ty'])
+# print(myDict5)  #{'q': 'w', 'e': 'r', 't': 'y'}
+
+# keyList=['a','b']
+# valueList=[111,222]
+
+# myDict6=dict(zip(keyList,valueList))
+
+# print(myDict6)  #{'a': 111, 'b': 222}
+
+
+# myDict7=dict(firstName='Joe', lastName='Smith')
+# print(myDict7)  #{'firstName': 'Joe', 'lastName': 'Smith'}
+
+
+
+
+
+
+
+# myDict1= {'key1': 1, 'key2': 20.5, 'key3': True}
+# myDict2= {1: 'student', 2: 'admin'}
+
+# print(myDict1)  #{'key1': 1, 'key2': 20.5, 'key3': True}
+# print(myDict2)  #{1: 'student', 2: 'admin'}
+
+# bookDict={'author':'Eric Matthes',
+#         'title':'Python Crash Course',
+#         'price':14.43,
+#         'reading age':'12 years and up',
+#         'language':'English'
+#         }
+
