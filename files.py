75e849cb23332a6e66ba2902ac2053757bd540e7
Anchen <anchen.y@gmail.com>
2022-04-13 21:53:10 +0300

    contextManager

--- files.py
+++ files.py
@@ -1,11 +1,48 @@
-try:
-    print("We're opening a file...")
-    fileHandler = open("../Data/test.txt")
-    
-except:
-    print("We can't open a file. Sorry!")
+path1="../Data/example.txt"
+path2="../Data/result.txt"
 
-print("Program is finished. Bye!")
+with open(path1) as inFile, open(path2, "w") as outFile:
+    # read the content from example.txt
+    fileContents = inFile.read()
+    # transform the content
+    fileContents=fileContents.lower()
+    # write the transformed content to result.txt
+    outFile.write(fileContents)
+
+
+# with open('../Data/example.txt', 'w') as fileHandler:
+#   	fileHandler.write('Hello!!!')
+
+
+# with open('../Data/example.txt', 'r') as fileHandler:
+#     for line in fileHandler:
+#         print(line)
+
+# fileHandler = open('../Data/example.txt')
+# with fileHandler:
+#     fileContents = fileHandler.read()
+#     print(fileContents)
+
+
+# fileHandler = open("../Data/example.txt","a")
+# fileHandler.write("\nHel lo Again!")
+
+# fileHandler1 = open("../Data/example.txt")
+# print(fileHandler1.read())
+
+
+
+
+
+
+# try:
+#     print("We're opening a file...")
+#     fileHandler = open("../Data/test.txt")
+    
+# except:
+#     print("We can't open a file. Sorry!")
+
+# print("Program is finished. Bye!")
 
 # with open("../Data/newTest.txt") as f:
 #    # perform file operations
