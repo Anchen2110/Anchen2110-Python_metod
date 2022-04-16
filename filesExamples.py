9c716a5dd7ee9ccfc0c3ef0e51263ffb36744fb5
anna <egoshina_a@itstep.academy>
2022-04-16 21:52:27 +0300

    1

--- filesExamples.py
+++ filesExamples.py
@@ -1,3 +1,55 @@
+def removeLine(fileIn, fileOut, lineNumber):
+    with open(fileIn) as fr:
+        lines = fr.readlines()
+
+        counter=1 # position pointer 
+
+        with open(fileOut, 'w') as fw:
+            for line in lines:
+                if counter != lineNumber:
+                    fw.write(line)
+                counter += 1
+
+myFile='../Data/Info.txt'
+resultFile='../Data/result.txt'
+
+removeLine(myFile, resultFile, 2)
+
+
+
+
+def readFromFile(fileName):
+    with open(fileName) as fileHandler:
+        data = fileHandler.read()
+        print(data)
+
+def removePunctuation(myStr, marks):
+    resultStr=""
+    for symbol in myStr:
+        if symbol not in marks:
+            resultStr+=symbol
+    return resultStr
+            
+
+
+def reverseFileWords(fileName):
+    with open(fileName) as fileHandler:
+        data = fileHandler.read()
+        data=removePunctuation(data, punctuationSymbols)
+        words=data.split()
+        reversedWords=reversed(words)
+
+        for word in reversedWords:
+            print(word)
+
+    
+punctuationSymbols='''!()-;?@#$%:'"\,./*_'''
+myFile='../Data/simpleText.txt'
+
+reverseFileWords(myFile)
+
+
+
 def readFromFile(fileName):
     with open(fileName) as fileHandler:
         data = fileHandler.read()
@@ -24,29 +76,29 @@ print("Number of words: {}".format(wordCounter(myFile)))
 
 
 
-# def replaceTextInFile(fileName,originText,newText):
-#     with open(fileName) as fileHandler:
-#         data = fileHandler.read()
-#         data = data.replace(originText, newText)
+def replaceTextInFile(fileName,originText,newText):
+    with open(fileName) as fileHandler:
+        data = fileHandler.read()
+        data = data.replace(originText, newText)
 
-#     with open(fileName,'w') as fileHandler:
-#         fileHandler.write(data)
+    with open(fileName,'w') as fileHandler:
+        fileHandler.write(data)
 
-# def readFromFile(fileName):
-#     with open(fileName) as fileHandler:
-#         data = fileHandler.read()
-#         print(data)
+def readFromFile(fileName):
+    with open(fileName) as fileHandler:
+        data = fileHandler.read()
+        print(data)
        
     
-# myFile='../Data/PythonAbout.txt'
+myFile='../Data/PythonAbout.txt'
 
-# print("Original file content:")
-# readFromFile(myFile)
+print("Original file content:")
+readFromFile(myFile)
 
-# replaceTextInFile(myFile,'Python','Java Script')
+replaceTextInFile(myFile,'Python','Java Script')
 
-# print("New file content:")
-# readFromFile(myFile)
+print("New file content:")
+readFromFile(myFile)
 
 
 
