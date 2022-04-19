1a537b41cfb2d6d4009bb4a0d1f27aaedb5b6002
anna <egoshina_a@itstep.academy>
2022-04-19 22:59:38 +0300

    111

--- oop_9_1.py
+++ oop_9_1.py
@@ -7,8 +7,22 @@
 # myVar="Hello"
 # print(type(myVar))
 
+# class Student:
+#     pass
+
 class Student:
-    pass
+    age=20,
+    name="Bob"
+
+   
+
+student1=Student()
+print(student1.name)
+
+student1.sayHello()
+
+
+
 
 
 
