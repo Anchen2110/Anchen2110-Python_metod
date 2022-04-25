35aacb4a3eec7744a4a816b5f8dda58bf183a183
anna <egoshina_a@itstep.academy>
2022-04-25 21:03:57 +0300

    25_4

--- oop_9_1.py
+++ oop_9_1.py
@@ -11,15 +11,40 @@
 #     pass
 
 class Student:
-    age=20,
-    name="Bob"
 
-   
+    spec="Computer science"
 
-student1=Student()
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def showInfo(self):
+        return f"Student {self.name} is {self.age} years old."
+
+    def showMsg(self, msgText):
+        return f"Student {self.name} says '{msgText}'."
+
+student1=Student("Bob",20)
+student2=Student("Jane", 18)
+
+print(student1.showInfo())
+print(student1.showMsg("Hello!"))
+
+print(student2.showInfo())
+print(student2.showMsg("Hi!"))
+
+print("Student's 1 info:")
 print(student1.name)
+print(student1.age)
+print(student1.spec)
 
-student1.sayHello()
+print("Student's 2 info:")
+print(student2.name)
+print(student2.age)
+print(student1.spec)
+
+
+# student1.sayHello()
 
 
 
