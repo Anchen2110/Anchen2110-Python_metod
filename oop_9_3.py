f51336a8d1b8f18dc5222bfa6dba4e28a014816c
anna <egoshina_a@itstep.academy>
2022-05-10 21:21:16 +0300

    1

--- oop_9_3.py
+++ oop_9_3.py
@@ -1,15 +1,15 @@
 class Person:
 
-    def __init__(self, firsName, lastName, age):
-        self.firsName = firsName
+    def __init__(self, firstName, lastName, age):
+        self.firstName = firstName
         self.lastName = lastName
         self.age = age
 
     def showInfo(self):
-        return f"Person first name -  {self.firsNname}; last name - {self.lastName}; age - {self.age}."
+        return f"Person first name -  {self.firstName}; last name - {self.lastName}; age - {self.age}."
 
     def sayHi(self, msgText):
-        return f"{msgText}! I am {self.firsNname}."
+        return f"{msgText}! I am {self.firstName}."
 
 
 # person1=Person("Joe","Black",30)
@@ -22,21 +22,65 @@ class Person:
 # print(person2.sayHi("Hello"))
 
 
+# class Student(Person):
+#     spec="Computer Science"
+
+#     def isSuccessful(self,meanScore):
+#         if meanScore>=75:
+#             return True
+#         else:
+#             return False
+
+# student1=Student("Joe","Black",30)
+
+# print(student1.showInfo())
+# print(student1.sayHi("Morning"))
+
+# print(f"Is {student1.firsNname} successful student?.{student1.isSuccessful(85)}")
+
+
+
+
+# class Student(Person):
+#     spec="Computer Science"
+
+#     def __init__(self, firstName, lastName, age, score):
+#         super().__init__(firstName, lastName, age)
+#         self.score = score
+
+#     def isSuccessful(self):
+#         if self.score>=75:
+#             return True
+#         else:
+#             return False
+
+# student1=Student("Joe","Black",30, 78)
+
+# print(student1.showInfo())
+# print(student1.sayHi("Morning"))
+
+# print(f"Is {student1.firstName} successful student?.{student1.isSuccessful()}")
+
+
 class Student(Person):
     spec="Computer Science"
 
-    def isSuccessful(self,meanScore):
-        if meanScore>=75:
+    def __init__(self, firstName, lastName, age, score):
+        super().__init__(firstName, lastName, age)
+        self.score = score
+
+    def showInfo(self):
+        return super().showInfo()+f"score - {self.score}"
+    
+    def isSuccessful(self):
+        if self.score>=75:
             return True
         else:
             return False
 
-student1=Student("Joe","Black",30)
+student1=Student("Joe","Black",30, 78)
 
 print(student1.showInfo())
-print(student1.sayHi("Morning"))
-
-print(f"Is {student1.firsNname} successful student?.{student1.isSuccessful(85)}")
 
 
 
