202b786725726ebf8247032b4e1a813cddaac3b7
Anchen <anchen.y@gmail.com>
2022-05-13 17:31:54 +0300

    1

--- oop_9_3_1.py
+++ oop_9_3_1.py
@@ -1,63 +1,157 @@
-import random
-class Person:
+# import random
+# class Person:
 
-    def __init__(self, firstName, lastName, age):
-        # public properties
-        self.firstName = firstName
-        self.lastName = lastName
+#     hobby="Cooking"
 
-        #protecte properties
-        self._age = age
+#     def __init__(self, firstName, lastName, age):
+#         # public properties
+#         self.firstName = firstName
+#         self.lastName = lastName
 
-        #private properties
-        self.__personID=random.randint(1,100)        
+#         #protecte properties
+#         self._age = age
 
-    #private methods
-    def __showID(self):
-        print (self.__personID)
+#         #private properties
+#         self.__personID=random.randint(1,100)        
+
+#     #private methods
+#     def __showID(self):
+#         print (self.__personID)
     
-    # public methods
-    def showInfo(self):
-        self.__showID()
-        return f"Person first name -  {self.firstName}; last name - {self.lastName}; age - {self._age}."
+#     # public methods
+#     def showInfo(self):
+#         self.__showID()
+#         return f"Person first name -  {self.firstName}; last name - {self.lastName}; age - {self._age}."
 
-    def sayHi(self, msgText):
-        print(self.showInfo())
-        return f"{msgText}! I am {self.firstName}."
+#     def sayHi(self, msgText):
+#         print(self.showInfo())
+#         return f"{msgText}! I am {self.firstName}."
+
+#     #static methods
+#     @staticmethod
+#     def sayGreetings():
+#         print("Nice to meet you!")
+
+#     #class methods
+#     @classmethod
+#     def setDefaultHobby(cls):
+#         cls.hobby="Drawing"
+
+
 
 # person1=Person("Joe","Black",30)
+# print(person1.hobby)
+
+# Person.setDefaultHobby()
+
+# person2=Person("Kate","Smith",20)
+# print(person2.hobby)
+
+# person3=Person("Bob","Gordon",40)
+# print(person3.hobby)
+
+
+# import random
+# class Person:
+
+#     def __init__(self, firstName, lastName, age):
+#         # public properties
+#         self.firstName = firstName
+#         self.lastName = lastName
+
+#         #protecte properties
+#         self._age = age
+
+#         #private properties
+#         self.__personID=random.randint(1,100)        
+
+#     #private methods
+#     def __showID(self):
+#         print (self.__personID)
+    
+#     # public methods
+#     def showInfo(self):
+#         self.__showID()
+#         return f"Person first name -  {self.firstName}; last name - {self.lastName}; age - {self._age}."
+
+#     def sayHi(self, msgText):
+#         print(self.showInfo())
+#         return f"{msgText}! I am {self.firstName}."
+
+#     #static methods
+#     @staticmethod
+#     def sayGreetings():
+#         print("Nice to meet you!")
+
+# person1=Person("Joe","Black",30)
+# person1.sayGreetings()
+
+
+
+# import random
+# class Person:
+
+#     def __init__(self, firstName, lastName, age):
+#         # public properties
+#         self.firstName = firstName
+#         self.lastName = lastName
+
+#         #protecte properties
+#         self._age = age
+
+#         #private properties
+#         self.__personID=random.randint(1,100)        
+
+#     #private methods
+#     def __showID(self):
+#         print (self.__personID)
+    
+#     # public methods
+#     def showInfo(self):
+#         self.__showID()
+#         return f"Person first name -  {self.firstName}; last name - {self.lastName}; age - {self._age}."
+
+#     def sayHi(self, msgText):
+#         print(self.showInfo())
+#         return f"{msgText}! I am {self.firstName}."
+
+#     def sayGreetings():
+#         print("Nice to meet you!")
+
+# person1=Person("Joe","Black",30)
+# person1.sayGreetings()
 # print(person1.showInfo())
 
 
 
-class Employee(Person):
-    def __init__(self, firstName, lastName, age, salary):
-        super().__init__(firstName, lastName, age)
-        self.salary = salary
+# class Employee(Person):
+#     def __init__(self, firstName, lastName, age, salary):
+#         super().__init__(firstName, lastName, age)
+#         self.salary = salary
 
-    def isRetiree(self):
-        print(self.showInfo())
-        if self._age>60:
-            print(f"{self.firstName} is retiree")
-        else:
-            print(f"{self.firstName} is not retiree")
+#     def isRetiree(self):
+#         print(self.showInfo())
+#         if self._age>60:
+#             print(f"{self.firstName} is retiree")
+#         else:
+#             print(f"{self.firstName} is not retiree")
 
-    def changeAge(self, newAge):
-        self._age=newAge
+#     def changeAge(self, newAge):
+#         self._age=newAge
 
-    def showEmployeeID(self):
-        self.__showID()
+#     def showEmployeeID(self):
+#         self.__showID()
 
         #print(self.__personID)
 
 
-employee1=Employee("Joe","Black",30, 3000)
-employee1.isRetiree()
+# employee1=Employee("Joe","Black",30, 3000)
+# employee1.isRetiree()
 
-employee1.showEmployeeID()
+# employee1.showEmployeeID()
 
-employee1.changeAge(65)
-employee1.isRetiree()
+# employee1.changeAge(65)
+# employee1.isRetiree()
 
 
 # employee1._age=20
