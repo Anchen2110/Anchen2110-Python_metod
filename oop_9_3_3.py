3f5da82e466b81625d615d1dce740a106f5b0372
Anchen <anchen.y@gmail.com>
2022-05-16 11:01:06 +0300

    16

--- oop_9_3_3.py
+++ oop_9_3_3.py
@@ -1,34 +1,53 @@
-class myBook:
-    def __init__(self, title, author, pages):
-        self.title = title
-        self.author = author
-        self.pages = pages
+class myClass1:
+    def sayHi(self):
+        print("Hi from Class1")
 
-    def showBookInfo(self):
-        print("Title: {}".format(self.title))
-        print("Author: {}".format(self.author))
-        print("Pages: {}".format(self.pages))
+class myClass2(myClass1):
+    def sayHi(self):
+        print("Hi from Class2")
 
-class myFile:
-    def __init__(self, fileSize, src):
-        self.fileSize = fileSize
-        self.src = src
+class myClass3(myClass1):
+    def sayHi(self):
+        print("Hi from Class3")   
+
+class myClass4(myClass2, myClass3):
+    pass      
+
+myObj = myClass4()
+myObj.sayHi()
+
+# class myBook:
+#     def __init__(self, title, author, pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+
+#     def showInfo(self):
+#         print("Title: {}".format(self.title))
+#         print("Author: {}".format(self.author))
+#         print("Pages: {}".format(self.pages))
+
+# class myFile:
+#     def __init__(self, fileSize, src):
+#         self.fileSize = fileSize
+#         self.src = src
         
 
-    def showFileProps(self):
-        print("File size: {}".format(self.fileSize))
-        print("File source: {}".format(self.src))
+#     def showInfo(self):
+#         print("File size: {}".format(self.fileSize))
+#         print("File source: {}".format(self.src))
 
-class myEBook(myBook,myFile):
-    def __init__(self,title, author, pages,fileSize, src):
-        myBook.__init__(self,title, author, pages)
-        myFile.__init__(self,fileSize, src)
+# class myEBook(myBook,myFile):
+#     def __init__(self,title, author, pages,fileSize, src):
+#         myBook.__init__(self,title, author, pages)
+#         myFile.__init__(self,fileSize, src)
         
 
 
-eBook1=myEBook("Python Crash Course","Eric Matthes", 624, 1024,"https://www.amazon.co.uk/dp/1593276036/?tag=adnruk-21")
-eBook1.showBookInfo()
-eBook1.showFileProps()
+# print(myEBook.mro())
+# eBook1=myEBook("Python Crash Course","Eric Matthes", 624, 1024,"https://www.amazon.co.uk/dp/1593276036/?tag=adnruk-21")
+# eBook1.showInfo()
+
 
 
 # class myBook:
