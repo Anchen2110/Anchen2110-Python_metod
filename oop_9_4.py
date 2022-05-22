1b7df306d679548262937048d3d38c029afff6b0
Anchen <anchen.y@gmail.com>
2022-05-23 01:11:13 +0300

    23_5

--- oop_9_4.py
+++ oop_9_4.py
@@ -4,15 +4,73 @@ class Book:
         self.author = author
         self.pages = pages
 
-    def showInfo(self):
-        print("Title: {}".format(self.title))
-        print("Author: {}".format(self.author))
-        print("Pages: {}".format(self.pages))
+    def __str__(self):
+        return f"Title: {self.title}, author: {self.author}, pages: {self.pages}"
 
+    def __eq__(self, otherObj):
+        if self.author==otherObj.author and self.title==otherObj.title:
+            return True
+        else:
+            return False
+    
+    def __gt__(self, otherObj):
+        if self.pages>otherObj.pages:
+            return True
+        else:
+            return False
+
+    
+    
 book1=Book("Python Crash Course","Eric Matthes", 624)
 book2=Book("JavaScript: The Good Parts","Douglas Crockford", 170)
+book3=Book("Python Crash Course","Eric Matthes", 700)
 
-print(book1+book2)
+print(book1==book3)
+print(book1>book2)
+
+# class Book:
+#     def __init__(self, title, author, pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+
+#     def __str__(self):
+#         return f"Title: {self.title}, author: {self.author}, pages: {self.pages}"
+    
+    
+# book1=Book("Python Crash Course","Eric Matthes", 624)
+# book2=Book("JavaScript: The Good Parts","Douglas Crockford", 170)
+
+# print(book1==book2) #False
+#print(book1)
+
+# class Class1:
+#     def __new__(cls):
+#         print("Hi! I am __new__ magic method.") 
+#         return super(Class1, cls).__new__(cls)
+    
+#     def __init__(self):
+#         print("Hi! I am __init__ magic method.")
+        
+
+# book1=Class1()
+
+
+# class Book:
+#     def __init__(self, title, author, pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+
+#     def showInfo(self):
+#         print("Title: {}".format(self.title))
+#         print("Author: {}".format(self.author))
+#         print("Pages: {}".format(self.pages))
+
+# book1=Book("Python Crash Course","Eric Matthes", 624)
+# book2=Book("JavaScript: The Good Parts","Douglas Crockford", 170)
+
+# print(book1+book2)
 
 # number1=2
 # number2=5.7
