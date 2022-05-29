993896b89478b1a1cdec36fe4fb3f4d9ed09733a
Anchen <anchen.y@gmail.com>
2022-05-29 22:33:12 +0300

    4

--- oop_9_5_2.py
+++ oop_9_5_2.py
@@ -1,19 +1,70 @@
-pricesUSD=[100.34,35,67.99,25.5]
-print(pricesUSD)
+class myDecorator:
+    def __init__(self,fn):
+        self.fn=fn
+    
+    def __call__(self, num1, num2):
+        return self.fn(num1, num2)**2
+	
 
-def changePriceDecorator_v1(myFunction):
-    print("Hello! Let's change your prices...")
-    def simpleWrapper(argList):
-        print("I've got list of prices with {} elements. Function starts working...".format(len(argList)))
-        resutl=myFunction(argList)
-        resutlwithDisc=list(map(lambda x: x*(1-0.15), resutl))
-        print("Let's set a discount..")
-        return resutlwithDisc
-    return simpleWrapper
+@myDecorator
+def addNums(x,y):
+    return x+y
 
-#pricesToGRN = changePriceDecorator_v1(toPriceNew)
-@changePriceDecorator_v1
-def toPriceNew(priceList):
-    return list(map(lambda x: x*27.5, priceList))
+print(addNums(2,3)) #25
 
-print(toPriceNew(pricesUSD))
+
+
+# def methodDecorator(method_to_decorate):
+#     def wrapper(self):
+#         print("General information:")
+#         method_to_decorate(self)
+#     return wrapper
+
+
+# class Book:
+#     def __init__(self, title, author, pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+    
+#     @methodDecorator
+#     def showInfo(self):
+#         print("Title: {}".format(self.title))
+#         print("Author: {}".format(self.author))
+#         print("Pages: {}".format(self.pages))
+
+
+# book1=Book("Python Crash Course","Eric Matthes", 624)
+# book1.showInfo()
+# class Book:
+#     def __init__(self, title, author, pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+
+#     def showInfo(self):
+#         print("Title: {}".format(self.title))
+#         print("Author: {}".format(self.author))
+#         print("Pages: {}".format(self.pages))
+
+
+
+# pricesUSD=[100.34,35,67.99,25.5]
+# print(pricesUSD)
+
+# def changePriceDecorator_v1(myFunction):
+#     print("Hello! Let's change your prices...")
+#     def simpleWrapper(argList):
+#         print("I've got list of prices with {} elements. Function starts working...".format(len(argList)))
+#         resutl=myFunction(argList)
+#         resutlwithDisc=list(map(lambda x: x*(1-0.15), resutl))
+#         print("Let's set a discount..")
+#         return resutlwithDisc
+#     return simpleWrapper
+
+# #pricesToGRN = changePriceDecorator_v1(toPriceNew)
+# @changePriceDecorator_v1
+# def toPriceNew(priceList):
+#     return list(map(lambda x: x*27.5, priceList))
+
+# print(toPriceNew(pricesUSD))
