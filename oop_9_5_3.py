57c44c7b7fbea7ac51a490e5e4fe0f1736d80560
Anchen <anchen.y@gmail.com>
2022-06-03 15:12:24 +0300

    1

--- oop_9_5_3.py
+++ oop_9_5_3.py
@@ -2,31 +2,118 @@ class Product:
     def __init__(self, name, price, discountPercentage=25):
         self.name = name
         self.price = price
-        self._discountPercentage = discountPercentage
+        self.__discountPercentage = discountPercentage
     
     def getDiscount(self):
-        return self._discountPercentage/100
+        return self.__discountPercentage
 
     def setDiscount(self,value):
-        self._discountPercentage=value*100    
+        if 0.1<=value<=0.75:
+            self.__discountPercentage=value*100
+        elif 10<=value<=75:
+             self.__discountPercentage=value
+        else:
+            print(f"Discoun value less than 10% or greater than 75% is not possible!")
     
     def showInfo(self):
-        print (f"name:{self.name}, price with discount:{self.price*(1-self.getDiscount())} grn.")
+        print (f"name:{self.name}, price with discount:{self.price*(1-self.getDiscount()/100)} grn.")
     
     def toUSD(self,usdExchRate):
         return self.price*(1-self.getDiscount())/usdExchRate
 
-
-item1=Product("Lipton Peach Iced", 42, 0.3)
-item2=Product("Pure Apple Juice", 28)
-
+    discountPercentage = property(
+        fget=getDiscount,
+        fset=setDiscount
+    )   
+ 
+item1=Product("Lipton Peach Iced", 42, 30)
 item1.showInfo()
-print(f"Price in USD$:{item1.toUSD(30)}")
-item2.showInfo()
-print(f"Price in USD$:{item2.toUSD(30)}")
 
-item1.setDiscount(0.5)
-print(f"New discount for {item1.name} - {item1.getDiscount()*100}%.")
+print(item1.discountPercentage)
+
+item1.discountPercentage=0.25
+print(item1.discountPercentage)
+
+item1.discountPercentage=60
+print(item1.discountPercentage)
+
+item1.discountPercentage=85
+print(item1.discountPercentage)
+
+
+
+
+
+
+
+
+# # item1.setDiscount(0.8)
+
+# class Product:
+#     def __init__(self, name, price, discountPercentage=25):
+#         self.name = name
+#         self.price = price
+#         self._discountPercentage = discountPercentage
+    
+#     def getDiscount(self):
+#         return self._discountPercentage/100
+
+#     def setDiscount(self,value):
+#         if 0.1<=value<=0.75:
+#             self._discountPercentage=value*100
+#         else:
+#             print(f"Discoun value less than 10% or greater than 75% is not possible!")
+
+    
+#     def showInfo(self):
+#         print (f"name:{self.name}, price with discount:{self.price*(1-self.getDiscount())} grn.")
+    
+#     def toUSD(self,usdExchRate):
+#         return self.price*(1-self.getDiscount())/usdExchRate
+
+
+# item1=Product("Lipton Peach Iced", 42, 30)
+# item1.showInfo()
+
+# # item1.setDiscount(0.8)
+
+# item1._discountPercentage=90
+# item1.showInfo()
+
+# item2=Product("Pure Apple Juice", 28, 0.8)
+# item2.showInfo()
+
+
+
+# class Product:
+#     def __init__(self, name, price, discountPercentage=25):
+#         self.name = name
+#         self.price = price
+#         self._discountPercentage = discountPercentage
+    
+#     def getDiscount(self):
+#         return self._discountPercentage/100
+
+#     def setDiscount(self,value):
+#         self._discountPercentage=value*100    
+    
+#     def showInfo(self):
+#         print (f"name:{self.name}, price with discount:{self.price*(1-self.getDiscount())} grn.")
+    
+#     def toUSD(self,usdExchRate):
+#         return self.price*(1-self.getDiscount())/usdExchRate
+
+
+# item1=Product("Lipton Peach Iced", 42, 30)
+# item2=Product("Pure Apple Juice", 28)
+
+# item1.showInfo()
+# print(f"Price in USD$:{item1.toUSD(30)}")
+# item2.showInfo()
+# print(f"Price in USD$:{item2.toUSD(30)}")
+
+# item1.setDiscount(0.5)
+# print(f"New discount for {item1.name} - {item1.getDiscount()*100}%.")
 
 # class Product:
 #     def __init__(self, name, price, discount=0.25):
