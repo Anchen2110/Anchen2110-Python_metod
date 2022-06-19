d481ed723509dc2216028204dd4f346dae7e891f
Anchen <anchen.y@gmail.com>
2022-06-20 01:09:01 +0300

    2

--- oop_9_6.py
+++ oop_9_6.py
@@ -15,37 +15,37 @@ print(myObj2.prop2) #100
 
 
 
-class MyClass1():
-    pass
+# class MyClass1():
+#     pass
 
-MyClass1=type("MyClass1",(),{})
+# MyClass1=type("MyClass1",(),{})
 
-myObj1 = MyClass1()
-print(myObj1)
-print(type(myObj1))
+# myObj1 = MyClass1()
+# print(myObj1)
+# print(type(myObj1))
 
 
 
-MyClass = type('MyClass', (BaseClass), clsDict)
+# MyClass = type('MyClass', (BaseClass), clsDict)
 
-class MyClass1():
-    pass
+# class MyClass1():
+#     pass
 
-myObj1 = MyClass1()
-print(myObj1)
-print(type(myObj1))
+# myObj1 = MyClass1()
+# print(myObj1)
+# print(type(myObj1))
 
-print(type(type(myObj1)))
+# print(type(type(myObj1)))
 
-myNumber=10
-myStr="Admin"
-myList=[1,2,3]
+# myNumber=10
+# myStr="Admin"
+# myList=[1,2,3]
 
-print(type(myNumber))
-print(type(type(myNumber)))
+# print(type(myNumber))
+# print(type(type(myNumber)))
 
-print(type(myStr))
-print(type(type(myStr)))
+# print(type(myStr))
+# print(type(type(myStr)))
 
-print(type(myList))
-print(type(type(myList)))
+# print(type(myList))
+# print(type(type(myList)))
--- oop_9_6_2.py
+++ oop_9_6_2.py
@@ -1,20 +1,149 @@
-class MyMetaClass1(type):
-    def __new__(cls, name, bases, dict):
-        print("'Hello' from__new__()!")
-        print(cls, name, bases, dict)
-        return type.__new__(cls, name, bases, dict)
+class MyMetaClass3(type):
+    def __new__(cls, name, bases, dict, **kwargs):
+        resultCls=super().__new__(cls, name, bases,dict)
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(resultCls, key, value)
+        return resultCls
     
-    def __init__(self, name, bases, dict):
-        print("'Hello' from__init__()!")
+    
 
-    def __call__(self, *args, **kwargs):
-        print("'Hello' from__call__()!")
+class User(metaclass=MyMetaClass3, firstName='Joe', age=15):
+    attr=100
 
-class MyClass1(metaclass=MyMetaClass1):
-    pass
+class Book(metaclass=MyMetaClass3, title='Python', price=15.78):
+    attr=100
+
+obj1=User()
+print(obj1.firstName)
+print(obj1.age)
+
+obj2=Book()
+print(obj2.title)
+print(obj2.price)
 
 
-obj=MyClass1()
+
+
+
+# class MyMetaClass2(type):
+#     def __new__(cls, name, bases, dict):
+#         resultCls=super().__new__(cls, name, bases, dict)
+#         if 'id' not in dict.keys():
+#             print(f"No 'id' attr. in the class '{name}'! Let's add it.")
+#             resultCls.id=0       
+#         return resultCls
+
+    
+    
+
+# class User(metaclass=MyMetaClass2):
+#     attr=100
+
+# obj=User()
+# print(obj.id)
+
+
+# class MyMetaClass1(type):
+#     def __new__(cls, name, bases, dict):
+#         if 'id' not in dict.keys(): 
+#             print(f"No 'id' attr. in the class '{name}'! ")
+#         else:
+#             nMethods = {key: value for key, value in dict.items() if callable(value)}
+#             if len(nMethods)>3:
+#                 print(f"More than 3 methods in the class '{name}'! ")
+#             else:
+#                 print(f"Class '{name}' is creating...")            
+#                 return super().__new__(cls, name, bases, dict)
+    
+ 
+# class MyClass1(metaclass=MyMetaClass1):
+#     attr=100
+
+# class MyClass2(metaclass=MyMetaClass1):
+#     attr=100
+#     id=1
+
+#     def method1(self):
+#         pass
+
+#     def method2(self):
+#         pass
+
+#     def method3(self):
+#         pass
+
+# class MyClass3(metaclass=MyMetaClass1):
+#     id=2
+
+#     def method1(self):
+#         pass
+
+#     def method2(self):
+#         pass
+
+#     def method3(self):
+#         pass
+
+#     def method4(self):
+#         pass
+    
+
+
+
+
+# class MyMetaClass1(type):
+#     def __new__(cls, name, bases, dict):
+#         if 'id' not in dict.keys(): 
+#             print(f"No 'id' attr. in the class '{name}'! ")
+#         else:
+#             print(f"Class '{name}' is creating...")            
+#             return super().__new__(cls, name, bases, dict)
+    
+ 
+# class MyClass1(metaclass=MyMetaClass1):
+#     attr=100
+
+# class MyClass2(metaclass=MyMetaClass1):
+#     attr=100
+#     id=1
+# class MyMetaClass1(type):
+#     def __new__(cls, name, bases, dict):
+#         print("'Hello' from__new__()!")
+#         print("Type of the class being created ", cls)
+#         print("Name of the class being created:", name)
+#         print("Tuple of base classes: ", bases)
+#         print("Dictionary of attr.: ", dict)
+       
+#         return super().__new__(cls, name, bases, dict)
+    
+ 
+# class MyClass1(metaclass=MyMetaClass1):
+#     attr=100
+
+# class MyMetaClass1(type):
+#     def __new__(cls, name, bases, dict):
+#         print("'Hello' from__new__()!")
+#         print("Type of the class being created ", cls)
+#         print("Name of the class being created:", name)
+#         print("Tuple of base classes: ", bases)
+#         print("Dictionary of attr.: ", dict)
+       
+#         return type.__new__(cls, name, bases, dict)
+    
+ 
+# class MyClass1(metaclass=MyMetaClass1):
+#     attr=100
+
+
+#obj=MyClass1()
+
+   # def __init__(self, name, bases, dict):
+    #     print("'Hello' from__init__()!")
+
+    # def __call__(self, *args, **kwargs):
+    #     print("'Hello' from__call__()!")
+
 
 # class MyMetaClass1(type):
 #     pass
